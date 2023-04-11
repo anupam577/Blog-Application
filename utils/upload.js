ac7934f209fc8a1e7d898889028ddd9a0107bf03
Anupam singh <91001107+anupam577@users.noreply.github.com>
2023-04-11 18:55:53 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -1,8 +1,8 @@
 import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
-
+ import mongoose from 'mongoose';
 const storage = new GridFsStorage({
-    url: `mongodb://user:codeforinterview@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`,
+   url: process.env.MONGO_URI,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
